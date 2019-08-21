267360b263ca799d3692d65fda2073b491df07f1
Lucas Livchits <livchitsl@gmail.com>
2019-08-21 12:09:33 -0300

    Crea kindleBueno.js

---
+++ kindleBueno.js
@@ -0,0 +1,41 @@
+class Kindle {
+  constructor() {
+    this.readBooks = 0;
+    this.notReadYetBooks = 0;
+    this._current = null;
+    this._next = null;
+    this._last = null;
+    this._library = [];
+  }
+
+  add(eBook) { }
+
+  finishCurrentBook() { }
+
+  get library() { }
+
+  get size() { }
+
+  get currentEBook() { }
+
+  set currentEBook() { }
+
+  filterBy(criteria) { }
+
+  search(keywords) { }
+
+  sortBy(criteria) { }
+}
+
+class Ebook {
+  constructor(title, author, genre, cover) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.cover = cover;
+    this.read = false;
+    this.readDate = null;
+  }
+
+  static isEqual(eBookA, eBookB) { }
+}
