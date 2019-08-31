6f4f52cf0ce7d00ff29f2428584fc2c74ddda60b
mborroni <miliborroni@gmail.com>
2019-08-31 14:32:30 -0300

    Agrega .prettierrc

--- kindleBueno.js
+++ kindleBueno.js
@@ -9,7 +9,7 @@ class Kindle {
     this._recentSearches = [];
   }
 
-  add(eBook) { 
+  add(eBook) {
     if (this._exists(eBook)) {
       console.warn(`"${eBook.title}" already exists in library`);
       return;
@@ -33,12 +33,15 @@ class Kindle {
   }
 
   _updateNotReadYetBooks() {
-    this.notReadYetBooks = this._library.reduce((totalBooksNotRead, currentBook) => {
-      if (!currentBook.read) {
-        totalBooksNotRead++;
-      }
-      return totalBooksNotRead;
-    }, 0);
+    this.notReadYetBooks = this._library.reduce(
+      (totalBooksNotRead, currentBook) => {
+        if (!currentBook.read) {
+          totalBooksNotRead++;
+        }
+        return totalBooksNotRead;
+      },
+      0
+    );
   }
 
   _updateReadBooks() {
@@ -47,28 +50,32 @@ class Kindle {
         totalBooksRead++;
       }
       return totalBooksRead;
-    }, 0)
+    }, 0);
   }
 
   finishCurrentBook() {
     if (this._current === null) {
-      console.error('There is no current book to finish, you must add one first.');
+      console.error(
+        "There is no current book to finish, you must add one first."
+      );
       return;
     }
     this._current._read = true;
     this._current._readDate = Date.now();
     this._last = this._current;
     this._current = this._next;
-    this._next = this.library.find(eBooK => eBooK._read === false && !Ebook.isEqual(eBooK, this._current));
+    this._next = this.library.find(
+      eBooK => eBooK._read === false && !Ebook.isEqual(eBooK, this._current)
+    );
     this._updateReadBooks;
     this._updateNotReadYetBooks;
   }
 
-  get library() { }
+  get library() {}
 
-  get size() { }
+  get size() {}
 
-  get currentEBook() { }
+  get currentEBook() {}
 
   set currentEBook(eBook) {
     if (!this._exists(eBook)) {
@@ -82,31 +89,35 @@ class Kindle {
     }
   }
 
-  filterBy(criteria) { }
+  filterBy(criteria) {}
 
   search(keywords) {
     const searchKeywords = keywords.toLowerCase().trim();
     const result = this._library.filter(eBook => {
-      eBook.title.toLowerCase().includes(searchKeywords) || eBook.author.toLowerCase().includes(searchKeywords)
+      eBook.title.toLowerCase().includes(searchKeywords) ||
+        eBook.author.toLowerCase().includes(searchKeywords);
     });
-    
+
     this._recentSearches.push(searchKeywords);
-    if (this._recentSearches.length > 5) this._recentSearches.shift(); 
-    
-    return result.length > 0 ? result : console.log('There are no results found in your library');
+    if (this._recentSearches.length > 5) this._recentSearches.shift();
+
+    return result.length > 0
+      ? result
+      : console.log("There are no results found in your library");
   }
 
   sortBy(criteria) {
     const sortedArray = [...this._library];
-    const sortCriteria = (bookA, bookB) => bookA[criteria].localeCompare(bookB[criteria]);
+    const sortCriteria = (bookA, bookB) =>
+      bookA[criteria].localeCompare(bookB[criteria]);
 
-    if (criteria === 'author' || criteria === 'title') {
+    if (criteria === "author" || criteria === "title") {
       return sortedArray.sort(sortCriteria);
     }
 
     return console.warn('Criteria must be either "author" or "title"');
   }
-  
+
   get recentSearches() {
     return this._recentSearches;
   }
@@ -119,10 +130,14 @@ class Ebook {
     this.genre = genre;
     this.cover = cover;
     this.read = false;
-    this.readDate = null; 
+    this.readDate = null;
   }
 
   static isEqual(eBookA, eBookB) {
-    return eBookA.title === eBookB.title && eBookA.author === eBookB.author && eBookA.genre === eBookB.genre;
+    return (
+      eBookA.title === eBookB.title &&
+      eBookA.author === eBookB.author &&
+      eBookA.genre === eBookB.genre
+    );
   }
 }
